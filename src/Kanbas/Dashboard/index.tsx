de13c399e53a73b1874aa2c93399ac7ea5cd8dbf
Adam Jackson <ajax@nwnk.net>
2024-07-17 23:13:34 -0400

    a2: Done with 4.5, I think
    The extra bit at the end of section 5, not yet.

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -3,111 +3,152 @@ export default function Dashboard() {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <img src="/images/ryu.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+      <div id="wd-dashboard-courses" className="row">
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
-              Fireballs: Theory and Practice
+              <img src="/images/ryu.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
+                Fireballs: Theory and Practice
+              </h5>
+              <p className="card-text">
+                Is this class difficult? It's down-right fierce!
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              Is this class difficult? It's down-right fierce!
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/honda.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
+              <img src="/images/honda.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
               Throwing Hands
+              </h5>
+              <p className="card-text">
+                Dive in belly first.
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              Dive in belly first.
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/blanka.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
+              <img src="/images/blanka.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
               The Electrical Martial Arts
+              </h5>
+              <p className="card-text">
+                Finally, a class to get excited about.
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              Finally, a class to get excited about.
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/guile.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
+              <img src="/images/guile.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
               Jet Pilotry for the Impatient
+              </h5>
+              <p className="card-text">
+                You'll be flying in a flash!
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              You'll be flying in a flash!
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/ken.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
-              Dracopyropugilism
+              <img src="/images/ken.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
+              Draco&shy;pyro&shy;pugilism
+              </h5>
+              <p className="card-text">
+                NOTE: Conflicts with F:T&P.
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              NOTE: Conflicts with F:T&P.
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/chun-li.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
+              <img src="/images/chun-li.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
               From Ballet to Ballistics
+              </h5>
+              <p className="card-text">
+                We got a real kick out of this class
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              We got a real kick out of this class
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/zangief.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
+              <img src="/images/zangief.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
               Basket Weaving
+              </h5>
+              <p className="card-text">
+                Snorkel and mask encouraged.
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              Snorkel and mask encouraged.
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/dhalsim.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
+
+        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+          <div className="card">
+            <a className="wd-dashboard-course-link text-decoration-none text-dark"
               href="#/Kanbas/Courses/1234/Home">
+              <img src="/images/dhalsim.png" width="100%" style={{ minWidth: 256 }} />
+              <div className="card-body">
+              <h5 className="wd-dashboard-course-title card-title">
               Cooking with Spice
+              </h5>
+              <p className="card-text">
+                This one will stretch you to your limits.
+              </p>
+              <button className="btn btn-primary"> Go </button>
+              </div>
             </a>
-            <p className="wd-dashboard-course-title">
-              This one will stretch you to your limits.
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
         </div>
+
       </div>
     </div>
 );}
